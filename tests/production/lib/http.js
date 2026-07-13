5663fd07ed82cf50c5043dcc13da26ee2937f07e
thenetlyst <thenetlyst@gmail.com>
2026-07-14 00:09:21 +0530

    Add per-endpoint latency metrics

--- tests/production/lib/http.js
+++ tests/production/lib/http.js
@@ -4,7 +4,14 @@ import { CONFIG } from "../config/config.js";
 import { expectStatus, parseJson } from "./helpers.js";
 import { Trend } from "k6/metrics";
 
-const endpointLatency = new Trend("endpoint_latency");
+const endpointTrends = {
+    start_attempt: new Trend("start_attempt_latency"),
+    list_questions: new Trend("list_questions_latency"),
+    get_question: new Trend("get_question_latency"),
+    submit_answer: new Trend("submit_answer_latency"),
+    reconstruct: new Trend("reconstruct_latency"),
+    finalize: new Trend("finalize_latency"),
+};
 /**
  * Generic GET request.
  */
@@ -23,7 +30,11 @@ export function get(url, headers, expectedStatus = 200, tags = {}) {
 
     expectStatus(response, expectedStatus);
 
-    endpointLatency.add(response.timings.duration, tags);
+    const trend = endpointTrends[tags.endpoint];
+
+    if (trend) {
+        trend.add(response.timings.duration);
+    }
 
     return {
         response,
@@ -59,7 +70,11 @@ export function post(
 
     expectStatus(response, expectedStatus);
 
-    endpointLatency.add(response.timings.duration, tags);
+    const trend = endpointTrends[tags.endpoint];
+
+    if (trend) {
+        trend.add(response.timings.duration);
+    }
 
     return {
         response,
